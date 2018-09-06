a340d6f0b481da340bb482a1e1ce5a7ebdf57e8c
Pablo Arellano <pablo.arellanos97@gmail.com>
2018-09-05 23:42:42 -0700

    add item

--- ShoppingList/Controllers/ItemTableViewController.swift
+++ ShoppingList/Controllers/ItemTableViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 
 class ItemTableViewController: UITableViewController {
+    
+    var items: [Item] = Item.fake(10)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Shopping List Items"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -28,25 +32,42 @@ class ItemTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return items.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.name
+        
+        if item.isChecked {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
 
         return cell
     }
-    */
 
+    
+    @IBAction func AddItem(_ sender: Any) {
+        let alert = UIAlertController(title: "New Shopping List Item", message: "Enter item to add to shopping list: ", preferredStyle: .alert)
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(UIAlertAction(title: "Add", style: .default, handler: { (_) in
+            if let itemName = alert.textFields?[0].text {
+                let itemCount = self.items.count;
+                let item = Item(name: itemName)
+                self.items.append(item)
+                self.tableView.insertRows(at: [IndexPath(row: itemCount, section: 0)], with: .top)
+            }
+        }))
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
